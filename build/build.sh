390477fdf26b498aa94efeb59266500e2260b258
danielriesner <daniel.riesner@endocode.com>
2018-02-12 16:25:46 +0100

    Update build.sh

--- build/build.sh
+++ build/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-cd $GOPATH/src/github.com/QMSTR/qmstr
-dep ensure
-cd cmd
-go generate qmstr-master
+(cd $GOPATH/src/github.com/QMSTR/qmstr; dep ensure)
+go generate github.com/QMSTR/qmstr/cmd/qmstr-master
 
-go install qmstr-master
-go install qmstr-wrapper
-go install qmstr-cli
+(cd $GOPATH/src/github.com/QMSTR/qmstr; go test ./...)
+
+go install github.com/QMSTR/qmstr/cmd/qmstr-master
+go install github.com/QMSTR/qmstr/cmd/qmstr-wrapper
+go install github.com/QMSTR/qmstr/cmd/qmstr-cli
